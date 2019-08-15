019f9b4ca8b86efd1adae14f3780b5d724d3c7c7
Juha Matinmäki <dev@matinmaki.net>
2019-08-15 08:56:24 -0700

    Added Nagios check yum_check.sh

---
+++ nagios/yum_check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Yum update check for Nagios Remote Plugin Executor (NRPE) v0.1
+# Tested with CentOS 7, RHEL 7
+#
+#  Nagios exit codes:
+#  0 = OK
+#  1 = WARNING
+#  2 = CRITICAL
+#  3 = UNKNOWN
+#
+
+if [ -f /usr/bin/yum ]; then
+        OUTPUT=$(/usr/bin/yum -C --security check-update | grep " needed for security")
+        if /usr/bin/yum -C --security check-update | grep " needed for security" | grep -q "No packages"; then
+        echo "OK: $OUTPUT"
+        exit 0
+        else
+                echo "WARNING: $OUTPUT"
+                exit 1
+        fi
+else
+echo "Error: /usr/bin/yum not found"
+exit 3
+fi
